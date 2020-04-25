9cd238ad166ab4bc91c249a4bafd84f33cbfb250
Kiwane Garner <kiwanegarner012@yahoo.com>
2020-04-25 17:01:33 -0400

    updated the styles and layout for the manager rental page

--- resources/views/manageRentals.blade.php
+++ resources/views/manageRentals.blade.php
@@ -97,7 +97,7 @@
             Plus, when you list your property, you can accept rental applications for free to quickly screen prospective tenants. <a href="#">Learn more</a>
             </p>
             <p>
-                <smalll>
+                <small>
                     Manage a large apartment community? <br />
                     <a href="#">Learn more about advertising</a>
                 </small>
@@ -109,7 +109,7 @@
         </div>
     </div>
 
-    <div class="row" style="padding: 100px 0;">
+    <div class="row inverseRow" style="padding: 100px 0;">
         <div id="divManageApplicationsImg" class="col-sm">
         </div>
 
@@ -125,7 +125,7 @@
 
     <div class="row" style="padding: 100px 0;">       
         <div id="divStreamlinePayments" class="col-sm">
-            <h2>Streamline your payements</h2>
+            <h2>Streamline your payments</h2>
             <p>
             Get rent payments, utility payments and security deposits sent directly to your bank account, for all of your units, 
             and easily keep track of those payments in one place. <a href="#">Learn more</a>
@@ -135,6 +135,38 @@
 
         <div id="divStreamlinePaymentsImg" class="col-sm">
         </div>
+    </div>
+
+    <div class="row">
+         <div class="promoImage2 text-center" >
+             <div class="overlay2" style="height: 40vh;">
+                <h2>Save time with Zillow Rental Manager</h2>
+                <button class="zillowBtn">View your properties</button>
+            </div>
+         </div>
+    </div>
+
+    <div class="row" style="padding: 50px 0;">       
+        <div id="divStayInformed" class="col-sm">
+            <h2>Stay informed</h2>
+            <p>
+                Explore our Rentals Resource Center to learn more about managing your rental, get useful tips and tools, and dive into rental market trends.
+            </p>
+            <button>Learn more</button>
+        </div>
+
+        <div id="divHereToHelp" class="col-sm">
+            <h2>We're here to help</h2>
+            <p>
+                Visit our help center for commonly asked questions and ways to connect with our support team.
+            </p>
+            <button>Get help</button>
+        </div>
+    </div>
+    <hr />
+
+    <div class="row" style="padding: 50px 0; justify-content: center;">
+        <h4>Manage your properties from any device, anytime.</h3>
     </div>
 
      <script src="/js/app.js"></script>
